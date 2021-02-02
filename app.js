2f98fed24b9bf4681a686d81e51770e48045524c
DaeHyeonHong <fholng@outlook.com>
2021-02-02 23:06:35 +0900

    work

---
+++ app.js
@@ -0,0 +1,47 @@
+const canvas = document.querySelector('#jsCanvas');
+const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('.jsColor');
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
+
+function startPainting() {
+  painting = true;
+}
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  console.log(color);
+}
+
+if (canvas) {
+  canvas.addEventListener('mousemove', onMouseMove);
+  canvas.addEventListener('mousedown', startPainting);
+  canvas.addEventListener('mouseup', stopPainting);
+  canvas.addEventListener('mouseleave', stopPainting);
+}
+
+Array.from(colors).forEach(color =>
+  color.addEventListener('click', handleColorClick)
+);
