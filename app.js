af2c52e8c0d1be50ff7827b39953d4e76d26e826
DaeHyeonHong <fholng@outlook.com>
2021-02-03 02:37:30 +0900

    Fill Canvas

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const canvas = document.querySelector('#jsCanvas');
 const ctx = canvas.getContext('2d');
-const colors = document.getElementsByClassName('.jsColor');
+const colors = document.getElementsByClassName('jsColor');
+const range = document.querySelector('#jsRange');
+const mode = document.querySelector('#jsMode');
 
 canvas.width = 700;
 canvas.height = 700;
 
 ctx.strokeStyle = '#2c2c2c';
 ctx.lineWidth = 2.5;
+ctx.fillStyle = 'green';
+ctx.fillRect(50, 20, 100, 49);
 
 function startPainting() {
   painting = true;
 }
 
 let painting = false;
+let filling = false;
+
 
 function stopPainting() {
   painting = false;
@@ -32,7 +38,22 @@ function onMouseMove(event) {
 
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
-  console.log(color);
+  ctx.strokeStyle = color;
+}
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = 'Fill';
+  } else {
+    filling = true;
+    mode.innerText = 'Paint';
+  }
 }
 
 if (canvas) {
@@ -45,3 +66,12 @@ if (canvas) {
 Array.from(colors).forEach(color =>
   color.addEventListener('click', handleColorClick)
 );
+
+
+if (range) {
+  range.addEventListener('input', handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener('click', handleModeClick);
+}
